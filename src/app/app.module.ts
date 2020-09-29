4b4daf414d8e6a79052d5e131164380a7ecb3a57
Luis Barrios <nikeven@gmail.com>
2020-09-29 00:07:37 -0400

    Se conecta el boton de busqueda con la api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -30,6 +31,8 @@ import { CarouselModule } from 'ngx-owl-carousel-o';
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
     CarouselModule
   ],
   providers: [],
--- src/app/components/search-movies/search-movies.component.ts
+++ src/app/components/search-movies/search-movies.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+// Services
+import { SearchMoviesService } from '../../services/movies/search-movies.service';
+// Interfaces
+import { GetData } from '../../interfaces/get-data';
+import { Movie } from '../../interfaces/movie';
+
+import { environment } from '../../../environments/environment';
+
+
+interface Search {
+  movie:string;
+}
 
 @Component({
   selector: 'app-search-movies',
   templateUrl: './search-movies.component.html',
   styleUrls: ['./search-movies.component.scss']
 })
-export class SearchMoviesComponent implements OnInit {
 
-  constructor() { }
+export class SearchMoviesComponent implements OnInit {
+  listMovies: Movie[] = [];
+  searchMovie: string;
+
+  constructor(protected ssMovies: SearchMoviesService) { }
 
   ngOnInit(): void {
   }
 
+  searchMovies(f:Search){
+    let text = f.movie;
+    console.log('test', text)
+    this.ssMovies.searchMovies(text)
+    .subscribe(
+      (data: GetData) => {
+        console.log(data);
+      },
+      (error: any) => {
+        console.error(error);
+      }
+    );
+  }
 }
