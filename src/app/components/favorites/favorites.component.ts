90bdb02c6e5e13af53b529fe92b9b62eb63423ab
Luis Barrios <nikeven@gmail.com>
2020-09-29 16:13:53 -0400

    Maquetado inicial de favoritos. Se cumpleta funcion para eliminar pelicula de favoritos

--- src/app/components/favorites/favorites.component.ts
+++ src/app/components/favorites/favorites.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
+// Interfaces
+import { Movie } from '../../interfaces/movie';
 @Component({
   selector: 'app-favorites',
   templateUrl: './favorites.component.html',
   styleUrls: ['./favorites.component.scss']
 })
 export class FavoritesComponent implements OnInit {
+  listFavorites: Movie[] = [];
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+    this.favorites();
   }
 
+  favorites(): void{
+    if (localStorage.getItem('fav')){
+      this.listFavorites = JSON.parse(localStorage.getItem('fav'));
+    } else {
+      this.listFavorites = [];
+    }
+  }
+  removeFav(item: Movie): void{
+    this.listFavorites.splice(this.listFavorites.indexOf(item), 1);
+    localStorage.setItem('fav', JSON.stringify(this.listFavorites));
+  }
+  goHome(): void{
+    this.router.navigate(['']);
+  }
 }
