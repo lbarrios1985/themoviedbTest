f732efbbe83957e23d69727f2c7ceebd72cd9b08
Luis Barrios <nikeven@gmail.com>
2020-09-29 09:59:37 -0400

    Se completa el campo de busqueda

--- src/app/components/search-movies/search-movies.component.ts
+++ src/app/components/search-movies/search-movies.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 // Services
 import { SearchMoviesService } from '../../services/movies/search-movies.service';
 // Interfaces
@@ -9,7 +9,7 @@ import { environment } from '../../../environments/environment';
 
 
 interface Search {
-  movie:string;
+  movie: string;
 }
 
 @Component({
@@ -21,23 +21,51 @@ interface Search {
 export class SearchMoviesComponent implements OnInit {
   listMovies: Movie[] = [];
   searchMovie: string;
+  API_BASE_IMAGE: string;
+  imageSize: string;
 
-  constructor(protected ssMovies: SearchMoviesService) { }
+  @ViewChild('search', { static: false }) search: any;
+
+  constructor(protected ssMovies: SearchMoviesService) {
+    this.API_BASE_IMAGE = environment.API_BASE_IMAGE;
+  }
 
   ngOnInit(): void {
   }
 
-  searchMovies(f:Search){
-    let text = f.movie;
-    console.log('test', text)
-    this.ssMovies.searchMovies(text)
-    .subscribe(
-      (data: GetData) => {
-        console.log(data);
-      },
-      (error: any) => {
-        console.error(error);
-      }
-    );
+  searchMovies(f: Search): void{
+    const text = f.movie;
+    const error = document.getElementById('error_search');
+    if (text !== '') {
+      this.ssMovies.searchMovies(text)
+      .subscribe(
+        (data: GetData) => {
+          this.imageSize = 'w500';
+          const count = data.results.length >= 9 ? 9 : data.results.length;
+          if (count === 0) {
+            this.listMovies = [];
+            console.log(error);
+            error.innerHTML = 'Movie not found, sorry!';
+          } else {
+            for (let i = 0; i < 9; i++) {
+              data.results[i].poster_path = this.API_BASE_IMAGE + this.imageSize + data.results[i].poster_path;
+              data.results[i].backdrop_path = this.API_BASE_IMAGE + this.imageSize + data.results[i].backdrop_path;
+              this.listMovies.push(data.results[i]);
+            }
+          }
+        },
+        (error: any) => {
+          console.error(error);
+        }
+      );
+    } else{
+      error.innerHTML = '';
+    }
+  }
+  clearSearch(): void{
+    const input = document.getElementById('search');
+    input.addEventListener('search', (event): void => {
+      this.listMovies = [];
+    });
   }
 }
